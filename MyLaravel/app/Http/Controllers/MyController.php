54f4a0fba656327a11c8f8ebe3e3219839a71806
Benito <bbxblack@gmail.com>
2021-05-23 16:51:24 +0200

    v0.7

--- MyLaravel/app/Http/Controllers/MyController.php
+++ MyLaravel/app/Http/Controllers/MyController.php
@@ -10,12 +10,41 @@ use Illuminate\Http\Request;
 // che restituisca una stringa riportante tutte le variabili; generare
 // poi un paio di istanze e stamparle per mezzo del dd() come visto in classe
 
+class Movie {
+
+    public $titolo;
+
+    public $descrizione;
+
+    public function _construct($titolo, $descrizione) {
+
+        $this -> titolo = $titolo;
+        $this -> descrizione = $descrizione == null ? 'nessuna descrizione' : $descrizione;
+    }
+
+    public function getString() {
+
+        return "Movie '" . $this-> titolo . "' :<br>" . $this-> descrizione;;
+    }
+
+}
+
 class MyController extends Controller
 {
     //
 
     public function home() {
 
-        return view('pages.home');
+        $filmUno = new Movie('Narnia', 'Smettiamola di nascondere i bimbi negli armadi!!!!!');
+        $filmDue = new Movie('Sono tornato');
+
+        $str1 = $filmUno -> getString();
+        $str2 = $filmDue -> getString();
+
+        dd($str1);
+
+        dd($str2);
+
+        return view('pages.home', compact('filmUno', 'filmDue'));
     }
 }
